d98b08b85d075be91b173887c447dfc1476b6cc1
VentsiKonov <ventsi.konov@gmail.com>
2014-11-07 21:35:12 +0200

    Field class backbone

---
+++ BrainPhucker/BrainPhucker/Field.cpp
@@ -0,0 +1,16 @@
+#include "Field.h"
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+class Field {
+public:
+
+private:
+	Field();
+	Field(const Field&);
+	void operator=(const Field&);
+
+
+};
